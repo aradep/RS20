82139656b4708c5b879c9746886655496308078d
Degen <degenesis0@gmail.com>
2025-10-01 23:31:54 -0300

    fix

--- data/sql/custom/db_world/NPC_482022_1v1BattlemasterCopy.sql
+++ data/sql/custom/db_world/NPC_482022_1v1BattlemasterCopy.sql
@@ -7,36 +7,7 @@
 -- Coliseum - 1v1 Arena battlemaster
 -- -----------------------------------
 
--- Creature 1
-SET
-@Entry      = 482021,
-@Model      = 29267,
-@Name       = "Sara",
-@Title      = "1v1 Battlemaster",
-@Icon       = "",
-@GossipMenu = 0,
-@Broadcast  = 0,
-@NpcText    = 23000,
-@MinLevel   = 80,
-@MaxLevel   = 80,
-@Faction    = 35,
-@NPCFlags   = 1048577,
-@Scale      = 0.5,
-@Rank       = 0,
-@Class      = 1,
-@UnitFlags  = 768,
-@UnitFlags2 = 2048,
-@Type       = 0,
-@TypeFlags  = 0,
-@FlagsExtra = 0,
-@AIName     = "",
-@Script     = "npc_1v1arena";
-DELETE FROM `creature_template` WHERE `entry` = @Entry;
-INSERT INTO `creature_template` (`entry`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `unit_class`, `unit_flags`, `type`, `type_flags`, `RegenHealth`, `flags_extra`, `AiName`, `ScriptName`) VALUES (@Entry, @Name, @Title, @Icon, @GossipMenu, @MinLevel, @MaxLevel, @Faction, @NPCFlags, 1, 1.14286, @Scale, @Rank, @Class, @UnitFlags, @Type, @TypeFlags, 1, @FlagsExtra, @AIName, @Script);
-DELETE FROM `creature_template_model` WHERE `CreatureID` = @Entry;
-INSERT INTO `creature_template_model` VALUES (@Entry, 0, @Model, @Scale, 1, 12340);
-
--- Creature 2
+-- Creature
 SET
 @Entry      = 482022,
 @Model      = 29267,
