6e9a4051eeaa1ba6cd83ef1b627ece71e061a8f1
Degen <degenesis0@gmail.com>
2025-10-07 07:23:54 -0300

    fix

--- db_auth/SERVER_Realmlist.sql
+++ db_auth/SERVER_Realmlist.sql
@@ -14,4 +14,4 @@ DELETE FROM `realmlist` WHERE `id`=1;
 INSERT INTO `realmlist` VALUES (1, 'Hall of Champions', '51.222.138.242', '127.0.0.1', '255.255.255.0', 8085, 1, 32, 2, 0, 0, 12340);
 
 -- Message of the day
-UPDATE `motd` SET `text`='Welcome to the HoC beta!' WHERE `realmid`=1;
+UPDATE `motd` SET `text`='Welcome to the HoC beta!' WHERE `realmid`=-1;
