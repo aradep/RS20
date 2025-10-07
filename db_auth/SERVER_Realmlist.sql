8698f97cf1280535f4bfe8582639632c7e32aa79
Degen <degenesis0@gmail.com>
2025-10-07 05:41:07 -0300

    fix

--- db_auth/SERVER_Realmlist.sql
+++ db_auth/SERVER_Realmlist.sql
@@ -1,17 +1,17 @@
---------------------------------------
+-- -----------------------------------
 -- Dragonshrine Instant-20 Funserver
 -- Made for AzerothCore 3.3.5a
 -- Author Discord: @Degen
 -- 2025
---------------------------------------
---------------------------------------
+-- -----------------------------------
+-- -----------------------------------
 
 -- Realm Changes
---------------------------------------
+-- -----------------------------------
 
 -- Realmlist
 DELETE FROM `realmlist` WHERE `id`=1;
-INSERT INTO `realmlist` VALUES (1, 'Dragonshrine', '127.0.0.1', '127.0.0.1', '255.255.255.0', 8085, 1, 32, 2, 0, 0, 12340);
+INSERT INTO `realmlist` VALUES (1, 'Hall of Champions', '51.222.138.242', '127.0.0.1', '255.255.255.0', 8085, 1, 32, 2, 0, 0, 12340);
 
 -- Message of the day
-UPDATE `motd` SET `text`='Welcome to Dragonshrine!' WHERE `realmid`=1;
+UPDATE `motd` SET `text`='Welcome to the HoC beta!' WHERE `realmid`=1;
