fa6007f65175621e00e557d6188553c2f9e54784
Degen <degenesis0@gmail.com>
2025-10-10 03:39:50 -0300

    drop rates

--- db_world/SERVER_Content_Dungeons.sql
+++ db_world/SERVER_Content_Dungeons.sql
@@ -21,7 +21,6 @@ INSERT INTO `game_graveyard` (`ID`, `Map`, `x`, `y`, `z`, `Comment`) VALUES
 (4, @DungeonMap, @DungeonX, @DungeonY, @DungeonZ, 'Custom Graveyard'),
 (1462,   @DungeonMap, @DungeonX, @DungeonY, @DungeonZ, 'Custom Graveyard');
 
-
 -- SFK
 SET
 @DungeonMap = 0,
@@ -66,8 +65,9 @@ INSERT INTO `game_graveyard` (`ID`, `Map`, `x`, `y`, `z`, `Comment`) VALUES
 (1, @DungeonMap, @DungeonX, @DungeonY, @DungeonZ, 'Custom Graveyard'),
 (106,   @DungeonMap, @DungeonX, @DungeonY, @DungeonZ, 'Custom Graveyard');
 
--- Loot changes
--- -----------------------------------
+-- Update drop chance of rares like Shadowfang to 0.5%
+UPDATE `creature_loot_template` clt JOIN `item_template` it ON clt.`item` = it.`entry`
+SET clt.`chance` = 0.5 WHERE clt.`groupid` = 0 AND clt.`chance` < 0.5 AND it.`itemlevel` BETWEEN 20 AND 30 AND it.`quality` = 3;
 
 -- Add 1x Darkmoon Card to end bosses
 DELETE FROM `creature_loot_template` WHERE `comment` LIKE '% - Darkmoon Card';
@@ -154,73 +154,73 @@ REPLACE INTO `creature_loot_template` VALUES
 (6243, 43102, 0, 100, 0, 1, 0, 1, 1, 'Gelihast - Frozen Orb'),
 (12902, 43102, 0, 100, 0, 1, 0, 1, 1, 'Lorgus Jett - Frozen Orb');
 
--- Add 20% chance Dream Shard to all trash mobs
+-- Add 10% chance Dream Shard to all trash mobs
 -- Shadowfang Keep
 DELETE FROM `creature_loot_template` WHERE `comment` LIKE '% - Small Dream Shard';
 REPLACE INTO `creature_loot_template` VALUES
-(2529, 34052, 0, 20, 0, 1, 0, 1, 1, 'Son of Arugal - Small Dream Shard'),
-(3851, 34052, 0, 20, 0, 1, 0, 1, 1, 'Shadowfang Whitescalp - Small Dream Shard'),
-(3853, 34052, 0, 20, 0, 1, 0, 1, 1, 'Shadowfang Moonwalker - Small Dream Shard'),
-(3854, 34052, 0, 20, 0, 1, 0, 1, 1, 'Shadowfang Wolfguard - Small Dream Shard'),
-(3855, 34052, 0, 20, 0, 1, 0, 1, 1, 'Shadowfang Darksoul - Small Dream Shard'),
-(3857, 34052, 0, 20, 0, 1, 0, 1, 1, 'Shadowfang Glutton - Small Dream Shard'),
-(3859, 34052, 0, 20, 0, 1, 0, 1, 1, 'Shadowfang Ragetooth - Small Dream Shard'),
-(3861, 34052, 0, 20, 0, 1, 0, 1, 1, 'Bleak Worg - Small Dream Shard'),
-(3862, 34052, 0, 20, 0, 1, 0, 1, 1, 'Slavering Worg - Small Dream Shard'),
-(3863, 34052, 0, 20, 0, 1, 0, 1, 1, 'Lupine Horror - Small Dream Shard'),
-(3864, 34052, 0, 20, 0, 1, 0, 1, 1, 'Fel Steed - Small Dream Shard'),
-(3866, 34052, 0, 20, 0, 1, 0, 1, 1, 'Vile Bat - Small Dream Shard'),
-(3868, 34052, 0, 20, 0, 1, 0, 1, 1, 'Blood Seeker - Small Dream Shard'),
-(3873, 34052, 0, 20, 0, 1, 0, 1, 1, 'Tormented Officer - Small Dream Shard'),
-(3875, 34052, 0, 20, 0, 1, 0, 1, 1, 'Haunted Servitor - Small Dream Shard'),
-(3877, 34052, 0, 20, 0, 1, 0, 1, 1, 'Wailing Guardsman - Small Dream Shard'),
+(2529, 34052, 0, 10, 0, 1, 0, 1, 1, 'Son of Arugal - Small Dream Shard'),
+(3851, 34052, 0, 10, 0, 1, 0, 1, 1, 'Shadowfang Whitescalp - Small Dream Shard'),
+(3853, 34052, 0, 10, 0, 1, 0, 1, 1, 'Shadowfang Moonwalker - Small Dream Shard'),
+(3854, 34052, 0, 10, 0, 1, 0, 1, 1, 'Shadowfang Wolfguard - Small Dream Shard'),
+(3855, 34052, 0, 10, 0, 1, 0, 1, 1, 'Shadowfang Darksoul - Small Dream Shard'),
+(3857, 34052, 0, 10, 0, 1, 0, 1, 1, 'Shadowfang Glutton - Small Dream Shard'),
+(3859, 34052, 0, 10, 0, 1, 0, 1, 1, 'Shadowfang Ragetooth - Small Dream Shard'),
+(3861, 34052, 0, 10, 0, 1, 0, 1, 1, 'Bleak Worg - Small Dream Shard'),
+(3862, 34052, 0, 10, 0, 1, 0, 1, 1, 'Slavering Worg - Small Dream Shard'),
+(3863, 34052, 0, 10, 0, 1, 0, 1, 1, 'Lupine Horror - Small Dream Shard'),
+(3864, 34052, 0, 10, 0, 1, 0, 1, 1, 'Fel Steed - Small Dream Shard'),
+(3866, 34052, 0, 10, 0, 1, 0, 1, 1, 'Vile Bat - Small Dream Shard'),
+(3868, 34052, 0, 10, 0, 1, 0, 1, 1, 'Blood Seeker - Small Dream Shard'),
+(3873, 34052, 0, 10, 0, 1, 0, 1, 1, 'Tormented Officer - Small Dream Shard'),
+(3875, 34052, 0, 10, 0, 1, 0, 1, 1, 'Haunted Servitor - Small Dream Shard'),
+(3877, 34052, 0, 10, 0, 1, 0, 1, 1, 'Wailing Guardsman - Small Dream Shard'),
 -- Stockade
-(1706, 34052, 0, 20, 0, 1, 0, 1, 1, 'Defias Prisoner - Small Dream Shard'),
-(1707, 34052, 0, 20, 0, 1, 0, 1, 1, 'Defias Captive - Small Dream Shard'),
-(1708, 34052, 0, 20, 0, 1, 0, 1, 1, 'Defias Inmate - Small Dream Shard'),
-(1711, 34052, 0, 20, 0, 1, 0, 1, 1, 'Defias Convict - Small Dream Shard'),
-(1715, 34052, 0, 20, 0, 1, 0, 1, 1, 'Defias Insurgent - Small Dream Shard'),
+(1706, 34052, 0, 10, 0, 1, 0, 1, 1, 'Defias Prisoner - Small Dream Shard'),
+(1707, 34052, 0, 10, 0, 1, 0, 1, 1, 'Defias Captive - Small Dream Shard'),
+(1708, 34052, 0, 10, 0, 1, 0, 1, 1, 'Defias Inmate - Small Dream Shard'),
+(1711, 34052, 0, 10, 0, 1, 0, 1, 1, 'Defias Convict - Small Dream Shard'),
+(1715, 34052, 0, 10, 0, 1, 0, 1, 1, 'Defias Insurgent - Small Dream Shard'),
 -- Deadmines
-(622, 34052, 0, 20, 0, 1, 0, 1, 1, 'Goblin Engineer - Small Dream Shard'),
-(634, 34052, 0, 20, 0, 1, 0, 1, 1, 'Defias Overseer - Small Dream Shard'),
-(636, 34052, 0, 20, 0, 1, 0, 1, 1, 'Defias Blackguard - Small Dream Shard'),
-(641, 34052, 0, 20, 0, 1, 0, 1, 1, 'Goblin Woodcarver - Small Dream Shard'),
-(647, 34052, 0, 20, 0, 1, 0, 1, 1, 'Captain Greenskin - Small Dream Shard'),
-(657, 34052, 0, 20, 0, 1, 0, 1, 1, 'Defias Pirate - Small Dream Shard'),
-(1729, 34052, 0, 20, 0, 1, 0, 1, 1, 'Defias Evoker - Small Dream Shard'),
-(1731, 34052, 0, 20, 0, 1, 0, 1, 1, 'Goblin Craftsman - Small Dream Shard'),
-(1732, 34052, 0, 20, 0, 1, 0, 1, 1, 'Defias Squallshaper - Small Dream Shard'),
-(3947, 34052, 0, 20, 0, 1, 0, 1, 1, 'Goblin Shipbuilder - Small Dream Shard'),
-(4417, 34052, 0, 20, 0, 1, 0, 1, 1, 'Defias Taskmaster - Small Dream Shard'),
-(4418, 34052, 0, 20, 0, 1, 0, 1, 1, 'Defias Wizard - Small Dream Shard'),
+(622, 34052, 0, 10, 0, 1, 0, 1, 1, 'Goblin Engineer - Small Dream Shard'),
+(634, 34052, 0, 10, 0, 1, 0, 1, 1, 'Defias Overseer - Small Dream Shard'),
+(636, 34052, 0, 10, 0, 1, 0, 1, 1, 'Defias Blackguard - Small Dream Shard'),
+(641, 34052, 0, 10, 0, 1, 0, 1, 1, 'Goblin Woodcarver - Small Dream Shard'),
+(647, 34052, 0, 10, 0, 1, 0, 1, 1, 'Captain Greenskin - Small Dream Shard'),
+(657, 34052, 0, 10, 0, 1, 0, 1, 1, 'Defias Pirate - Small Dream Shard'),
+(1729, 34052, 0, 10, 0, 1, 0, 1, 1, 'Defias Evoker - Small Dream Shard'),
+(1731, 34052, 0, 10, 0, 1, 0, 1, 1, 'Goblin Craftsman - Small Dream Shard'),
+(1732, 34052, 0, 10, 0, 1, 0, 1, 1, 'Defias Squallshaper - Small Dream Shard'),
+(3947, 34052, 0, 10, 0, 1, 0, 1, 1, 'Goblin Shipbuilder - Small Dream Shard'),
+(4417, 34052, 0, 10, 0, 1, 0, 1, 1, 'Defias Taskmaster - Small Dream Shard'),
+(4418, 34052, 0, 10, 0, 1, 0, 1, 1, 'Defias Wizard - Small Dream Shard'),
 -- Wailing Caverns
-(3636, 34052, 0, 20, 0, 1, 0, 1, 1, 'Deviate Ravager - Small Dream Shard'),
-(3637, 34052, 0, 20, 0, 1, 0, 1, 1, 'Deviate Guardian - Small Dream Shard'),
-(3640, 34052, 0, 20, 0, 1, 0, 1, 1, 'Evolving Ectoplasm - Small Dream Shard'),
-(3840, 34052, 0, 20, 0, 1, 0, 1, 1, 'Druid of the Fang - Small Dream Shard'),
-(5048, 34052, 0, 20, 0, 1, 0, 1, 1, 'Deviate Adder - Small Dream Shard'),
-(5056, 34052, 0, 20, 0, 1, 0, 1, 1, 'Deviate Dreadfang - Small Dream Shard'),
-(5755, 34052, 0, 20, 0, 1, 0, 1, 1, 'Deviate Viper - Small Dream Shard'),
-(5756, 34052, 0, 20, 0, 1, 0, 1, 1, 'Deviate Venomwing - Small Dream Shard'),
-(5761, 34052, 0, 20, 0, 1, 0, 1, 1, 'Deviate Shambler - Small Dream Shard'),
+(3636, 34052, 0, 10, 0, 1, 0, 1, 1, 'Deviate Ravager - Small Dream Shard'),
+(3637, 34052, 0, 10, 0, 1, 0, 1, 1, 'Deviate Guardian - Small Dream Shard'),
+(3640, 34052, 0, 10, 0, 1, 0, 1, 1, 'Evolving Ectoplasm - Small Dream Shard'),
+(3840, 34052, 0, 10, 0, 1, 0, 1, 1, 'Druid of the Fang - Small Dream Shard'),
+(5048, 34052, 0, 10, 0, 1, 0, 1, 1, 'Deviate Adder - Small Dream Shard'),
+(5056, 34052, 0, 10, 0, 1, 0, 1, 1, 'Deviate Dreadfang - Small Dream Shard'),
+(5755, 34052, 0, 10, 0, 1, 0, 1, 1, 'Deviate Viper - Small Dream Shard'),
+(5756, 34052, 0, 10, 0, 1, 0, 1, 1, 'Deviate Venomwing - Small Dream Shard'),
+(5761, 34052, 0, 10, 0, 1, 0, 1, 1, 'Deviate Shambler - Small Dream Shard'),
 -- Blackfathom Deeps
-(4798, 34052, 0, 20, 0, 1, 0, 1, 1, 'Fallenroot Shadowstalker - Small Dream Shard'),
-(4799, 34052, 0, 20, 0, 1, 0, 1, 1, 'Fallenroot Hellcaller - Small Dream Shard'),
-(4805, 34052, 0, 20, 0, 1, 0, 1, 1, 'Blackfathom Sea Witce - Small Dream Shard'),
-(4807, 34052, 0, 20, 0, 1, 0, 1, 1, 'Blackfathom Myrmidon - Small Dream Shard'),
-(4809, 34052, 0, 20, 0, 1, 0, 1, 1, 'Twilight Acolyte - Small Dream Shard'),
-(4810, 34052, 0, 20, 0, 1, 0, 1, 1, 'Twilight Reaver - Small Dream Shard'),
-(4811, 34052, 0, 20, 0, 1, 0, 1, 1, 'Twilight Aquamancer - Small Dream Shard'),
-(4812, 34052, 0, 20, 0, 1, 0, 1, 1, 'Twilight Loreseeker - Small Dream Shard'),
-(4813, 34052, 0, 20, 0, 1, 0, 1, 1, 'Twilight Shadowmage - Small Dream Shard'),
-(4814, 34052, 0, 20, 0, 1, 0, 1, 1, 'Twilight Elementalist - Small Dream Shard'),
-(4815, 34052, 0, 20, 0, 1, 0, 1, 1, 'Murkshallow Snapclaw - Small Dream Shard'),
-(4818, 34052, 0, 20, 0, 1, 0, 1, 1, 'Blindlight Murloc - Small Dream Shard'),
-(4819, 34052, 0, 20, 0, 1, 0, 1, 1, 'Blindlight Muckdweller - Small Dream Shard'),
-(4820, 34052, 0, 20, 0, 1, 0, 1, 1, 'Blindlight Oracle - Small Dream Shard'),
-(4821, 34052, 0, 20, 0, 1, 0, 1, 1, 'Skittering Crustacean - Small Dream Shard'),
-(4822, 34052, 0, 20, 0, 1, 0, 1, 1, 'Snapping Crustacean - Small Dream Shard'),
-(4823, 34052, 0, 20, 0, 1, 0, 1, 1, 'Barbed Crustacean - Small Dream Shard'),
-(4824, 34052, 0, 20, 0, 1, 0, 1, 1, 'Akumai Fisher - Small Dream Shard'),
-(4825, 34052, 0, 20, 0, 1, 0, 1, 1, 'Akumai Snapjaw - Small Dream Shard'),
-(4827, 34052, 0, 20, 0, 1, 0, 1, 1, 'Deep Pool Threshfin - Small Dream Shard');
+(4798, 34052, 0, 10, 0, 1, 0, 1, 1, 'Fallenroot Shadowstalker - Small Dream Shard'),
+(4799, 34052, 0, 10, 0, 1, 0, 1, 1, 'Fallenroot Hellcaller - Small Dream Shard'),
+(4805, 34052, 0, 10, 0, 1, 0, 1, 1, 'Blackfathom Sea Witce - Small Dream Shard'),
+(4807, 34052, 0, 10, 0, 1, 0, 1, 1, 'Blackfathom Myrmidon - Small Dream Shard'),
+(4809, 34052, 0, 10, 0, 1, 0, 1, 1, 'Twilight Acolyte - Small Dream Shard'),
+(4810, 34052, 0, 10, 0, 1, 0, 1, 1, 'Twilight Reaver - Small Dream Shard'),
+(4811, 34052, 0, 10, 0, 1, 0, 1, 1, 'Twilight Aquamancer - Small Dream Shard'),
+(4812, 34052, 0, 10, 0, 1, 0, 1, 1, 'Twilight Loreseeker - Small Dream Shard'),
+(4813, 34052, 0, 10, 0, 1, 0, 1, 1, 'Twilight Shadowmage - Small Dream Shard'),
+(4814, 34052, 0, 10, 0, 1, 0, 1, 1, 'Twilight Elementalist - Small Dream Shard'),
+(4815, 34052, 0, 10, 0, 1, 0, 1, 1, 'Murkshallow Snapclaw - Small Dream Shard'),
+(4818, 34052, 0, 10, 0, 1, 0, 1, 1, 'Blindlight Murloc - Small Dream Shard'),
+(4819, 34052, 0, 10, 0, 1, 0, 1, 1, 'Blindlight Muckdweller - Small Dream Shard'),
+(4820, 34052, 0, 10, 0, 1, 0, 1, 1, 'Blindlight Oracle - Small Dream Shard'),
+(4821, 34052, 0, 10, 0, 1, 0, 1, 1, 'Skittering Crustacean - Small Dream Shard'),
+(4822, 34052, 0, 10, 0, 1, 0, 1, 1, 'Snapping Crustacean - Small Dream Shard'),
+(4823, 34052, 0, 10, 0, 1, 0, 1, 1, 'Barbed Crustacean - Small Dream Shard'),
+(4824, 34052, 0, 10, 0, 1, 0, 1, 1, 'Akumai Fisher - Small Dream Shard'),
+(4825, 34052, 0, 10, 0, 1, 0, 1, 1, 'Akumai Snapjaw - Small Dream Shard'),
+(4827, 34052, 0, 10, 0, 1, 0, 1, 1, 'Deep Pool Threshfin - Small Dream Shard');
