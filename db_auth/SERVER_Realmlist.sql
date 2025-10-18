a9b0d7507694c17ba6cddb1461162f081a3ee472
Degen <degenesis0@gmail.com>
2025-10-18 16:43:14 -0300

    realmlist

--- db_auth/SERVER_Realmlist.sql
+++ db_auth/SERVER_Realmlist.sql
@@ -11,7 +11,7 @@
 
 -- Realmlist
 DELETE FROM `realmlist` WHERE `id`=1;
-REPLACE INTO `acore_auth`.`realmlist` VALUES (1, 'Dragonshrine', 'logon.rubysanctum.com', '127.0.0.1', '255.255.255.0', 8085, 1, 32, 2, 0, 0, 12340);
+INSERT INTO `acore_auth`.`realmlist` VALUES (1, 'Dragonshrine', 'logon.rubysanctum.com', '127.0.0.1', '255.255.255.0', 8085, 1, 32, 2, 0, 0, 12340);
 
 -- Message of the day
-UPDATE `motd` SET `text`='Welcome, champion.' WHERE `realmid`=-1;
+UPDATE `motd` SET `text`='Welcome to Dragonshrine!' WHERE `realmid`=-1;
